033e53b51fce2ec6ccca7ebc522a6bab6c1794c2
Luan Pessoa <devluan.pessoa@gmail.com>
2025-12-01 23:15:23 -0300

    refactor: remove unused Helm provider configuration and update README for EKS module

--- providers.tf
+++ providers.tf
@@ -25,18 +25,5 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.4"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.eks_cluster.token
   }
 }

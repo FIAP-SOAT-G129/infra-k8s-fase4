13b5c2daede0ef81ba45365c0b5d3b87a621544f
kelven <kelven.cleiton14@gmail.com>
2025-12-13 09:07:41 -0300

    Setup init

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ module "security_group" {
   source         = "./modules/security-group"
   name           = var.name
   vpc_id         = data.terraform_remote_state.foundation.outputs.vpc_id
-  vpc_cidr_block = data.terraform_remote_state.foundation.outputs.vpc_cidr_block
   tags           = var.tags
 }
 
 module "eks" {
-  source     = "./modules/eks"
-  name       = var.name
-  vpc_id     = data.terraform_remote_state.foundation.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.foundation.outputs.private_subnet_ids
-  tags       = var.tags
+  source             = "./modules/eks"
+  name               = var.name
+  vpc_id             = data.terraform_remote_state.foundation.outputs.vpc_id
+  subnet_ids         = data.terraform_remote_state.foundation.outputs.private_subnet_ids
+  security_group_ids = module.security_group.eks_sg_id
+  tags               = var.tags
 }

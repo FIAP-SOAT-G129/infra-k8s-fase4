9c42b9e83f96d2db6c0eafaa31a9c6ba0326b166
kelven <kelven.cleiton14@gmail.com>
2025-12-14 22:34:29 -0300

    fix cluster_name

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "eks" {
 
 module "alb" {
   source            = "./modules/alb"
-  name              = module.eks.cluster_name
+  cluster_name      = module.eks.cluster_name
   vpc_id            = data.terraform_remote_state.foundation.outputs.vpc_id
   oidc_provider     = module.eks.oidc_provider
   oidc_provider_arn = module.eks.oidc_provider_arn

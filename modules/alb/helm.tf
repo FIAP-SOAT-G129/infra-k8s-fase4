9d9829504a3bdd14887b12b979f1c0037c84710f
kelven <kelven.cleiton14@gmail.com>
2025-12-14 22:42:11 -0300

    fix fmt terraform

--- modules/alb/helm.tf
+++ modules/alb/helm.tf
@@ -9,22 +9,22 @@ resource "helm_release" "alb_controller" {
     kubernetes_service_account.alb
   ]
 
-set = [
-  {
-    name  = "clusterName"
-    value = var.name
-  },
-  {
-    name  = "vpcId"
-    value = var.vpc_id
-  },
-  {
-    name  = "serviceAccount.create"
-    value = "false"
-  },
-  {
-    name  = "serviceAccount.name"
-    value = kubernetes_service_account.alb.metadata[0].name
-  }
-]
+  set = [
+    {
+      name  = "clusterName"
+      value = var.name
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = kubernetes_service_account.alb.metadata[0].name
+    }
+  ]
 }

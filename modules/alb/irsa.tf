38d149af320b9e19ea37e7848a40ab59dfbe01dc
kelven <kelven.cleiton14@gmail.com>
2025-12-15 19:44:52 -0300

    fix refactor irsa.tf

--- modules/alb/irsa.tf
+++ modules/alb/irsa.tf
@@ -3,27 +3,22 @@ resource "aws_iam_role" "alb_controller" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = var.oidc_provider_arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          ("${replace(var.oidc_provider, "https://", "")}:sub") =
-          "system:serviceaccount:kube-system:aws-load-balancer-controller"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = var.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(var.oidc_provider, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
         }
       }
-    }]
+    ]
   })
 }
-
-resource "aws_iam_role_policy_attachment" "alb_attach" {
-  role       = aws_iam_role.alb_controller.name
-  policy_arn = aws_iam_policy.alb_controller.arn
-}
-
 
 resource "aws_iam_role_policy_attachment" "alb_attach" {
   role       = aws_iam_role.alb_controller.name
